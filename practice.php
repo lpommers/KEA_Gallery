9df48911cf3ea0f364fc741e9ff1433ab08df070
Luke Pommersheim <lpommers@gmail.com>
2013-11-11 15:01:19 +0100

    got the uploading to to work

--- practice.php
+++ practice.php
@@ -2,23 +2,31 @@
 	if ($_FILES) {
 
 		//moves uploaded file to permanent folder
-		$filename = $_FILES['file']['tmp_name'];
-		$destination = $_FILES['file']['name'];
+		$filename = $_FILES['filename']['tmp_name'];
+		$destination = $_FILES['filename']['name'];
 		move_uploaded_file($filename, $destination);
 
 		//save information
-		$link=mysql_connect('localhost', 'kriz001','kea660587#');
-		mysql_select_db('kriz001', $link);
+		$link=mysql_connect('localhost', 'kriz0001','kea660587#');
+		mysql_select_db('kriz0001', $link);
 
 		$username = $_POST['username'];
 		$title = $_POST['title'];
 		$description = $_POST['description'];
 		$location = $_POST['location'];
 
-		$sql = "INSERT INTO gallery (id, username, imagename, title, description, date, location) VALUES ('', '$username', '$destination', '$title', '$description', 'NOW()', $location)";
+		$sql = "INSERT INTO gallery (id, username, imagename, title, description, date, location) VALUES ('', '$username', '$destination', '$title', '$description', NOW(), '$location')";
 
 		$result = mysql_query($sql);
+		$out = '';
+			if ($result) {
+		$out .= 'Data has been inserted';
+	}else{
+		$out .= 'Data was NOT inserted';
+		$out .= mysql_error();
 
+	}
+	echo $out;
 	}
 
 ?>
@@ -30,7 +38,7 @@
 	<title>Gallery</title>
 </head>
 <body>
-    <form action = <?php $_SERVER['PHP_SELF']; ?> method="post" enctype = 'multipart/form-data'>
+    <form action = 'practice.php' method="post" enctype = 'multipart/form-data'>
     	User Name <input type="text" name="username"><br />
     	Image Title <input type="text" name="title"><br />
     	Image Description <textarea  name="description" cols=30 rows=10>
