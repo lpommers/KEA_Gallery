d7294a31410c362d21e8087b86e09b4e2f675674
Luke Pommersheim <lpommers@gmail.com>
2013-11-14 11:31:26 +0100

    full size image

--- fullimage.php
+++ fullimage.php
@@ -8,7 +8,16 @@
 	<link href='http://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<nav id="navigation">
+		<ul>
+			<li><a href="homepage.php">Gallery</a></li>
+		</ul>
+	</nav>
+
+	<h1>Here's your full size image</h1>
 	<div id = 'full_wrapper'>
+
+
 		<img id='image_full' src="<?php echo $_GET['imagename']; ?>" alt="">
 
 
