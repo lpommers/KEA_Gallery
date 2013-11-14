ac4a7087756a2d0065e6ac670f4fbe2d89271fbe
Luke Pommersheim <lpommers@gmail.com>
2013-11-14 11:25:40 +0100

    added full image

---
+++ fullimage.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width" />
+	<title>Full Screen image</title>
+	<link href="style.css" type="text/css" rel="stylesheet"/>
+	<link href='http://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id = 'full_wrapper'>
+		<img id='image_full' src="<?php echo $_GET['imagename']; ?>" alt="">
+
+
+
+	</div>
+</body>
+</html>
+
